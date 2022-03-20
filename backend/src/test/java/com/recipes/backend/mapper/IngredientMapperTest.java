d47f476474dff73cc535793c61dfe78e94b03bdd
soffi49 <zosia.wrona49@gmail.com>
2022-03-20 19:29:23 +0100

    [SE2R-41] Changed test class annotations

--- backend/src/test/java/com/recipes/backend/mapper/IngredientMapperTest.java
+++ backend/src/test/java/com/recipes/backend/mapper/IngredientMapperTest.java
@@ -7,13 +7,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class IngredientMapperTest {
 
     private Ingredient mockIngredient;
