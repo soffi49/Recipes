05048f55561962245e602b880d8d2b83d7f1de63
soffi49 <zosia.wrona49@gmail.com>
2022-04-06 18:01:16 +0200

    [SE2R-42] Tests corrected

--- backend/src/test/java/com/recipes/backend/rest/RecipeControllerUnitTest.java
+++ backend/src/test/java/com/recipes/backend/rest/RecipeControllerUnitTest.java
@@ -2,15 +2,19 @@ package com.recipes.backend.rest;
 
 import com.recipes.backend.bizz.recipe.RecipeService;
 import com.recipes.backend.bizz.recipe.domain.Recipe;
+import com.recipes.backend.bizz.security.SecurityService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.LongStream;
 
@@ -26,15 +30,25 @@ public class RecipeControllerUnitTest {
     @MockBean
     private static RecipeService recipeServiceMock;
 
+    @MockBean
+    private static SecurityService securityServiceMock;
+
+    private static String SECURITY_HEADER = "Header Mock";
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     @DisplayName("Get all recipes - correct parameters")
     void getAllRecipesCorrectParam() throws Exception {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("security_header", SECURITY_HEADER);
+
         Mockito.doReturn(setUpRecipeSet()).when(recipeServiceMock).getAllRecipes(0, 5);
+        Mockito.doReturn(true).when(securityServiceMock).isAuthenticated(httpHeaders);
 
         mockMvc.perform(get("/recipes")
+                .headers(httpHeaders)
                 .param("limit", "5")
                 .param("page", "0"))
                 .andExpect(status().isOk())
@@ -45,9 +59,13 @@ public class RecipeControllerUnitTest {
     @Test
     @DisplayName("Get all recipes - incorrect parameters")
     void getAllRecipesIncorrectParam() throws Exception {
-        Mockito.doReturn(setUpRecipeSet()).when(recipeServiceMock).getAllRecipes(0, 5);
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("security_header", SECURITY_HEADER);
 
-        mockMvc.perform(get("/recipes")
+        Mockito.doReturn(setUpRecipeSet()).when(recipeServiceMock).getAllRecipes(0, 5);
+        Mockito.doReturn(true).when(securityServiceMock).isAuthenticated(httpHeaders);
+
+        mockMvc.perform(get("/recipes").headers(httpHeaders)
                 .param("limit", "5")
                 .param("other", "0"))
                 .andExpect(status().isBadRequest());
