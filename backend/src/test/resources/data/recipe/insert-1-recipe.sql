f4b7d4fc62bbc1d3f381bb3ce77746570d08f963
Piotr Witkiewicz <piotr.witkiewicz.stud@pw.edu.pl>
2022-04-06 08:44:21 +0200

    SE2R-40: add integration tests to recipe controller

--- backend/src/test/resources/data/recipe/insert-1-recipe.sql
+++ backend/src/test/resources/data/recipe/insert-1-recipe.sql
@@ -7,7 +7,7 @@ TRUNCATE TABLE tag;
 SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO ingredient (ingredient_id, name) VALUES (1000, 'Ingredient');
-INSERT INTO tag (tag_id, name) VALUES (1000, 'vegetarian');
+INSERT INTO tag (tag_id, name) VALUES (1, 'vegetarian');
 INSERT INTO recipe (recipe_id, name, instructions) VALUES (1000, 'Recipe', 'Test instruction');
 INSERT INTO recipe_ingredient (recipe_id, ingredient_id, quantity) VALUES (1000, 1000, '10g');
-INSERT INTO recipe_tag (tag_id, recipe_id) VALUES (1000, 1000);
+INSERT INTO recipe_tag (tag_id, recipe_id) VALUES (1, 1000);
